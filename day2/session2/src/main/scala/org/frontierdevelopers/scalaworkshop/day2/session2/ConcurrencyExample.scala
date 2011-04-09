7ea7203e55fae7f0f2caf42424cb1e0e68f5a604
Sam Reid <reids@colorado.edu>
2011-04-09 13:04:45 -0600

    added remote actors

--- day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/ConcurrencyExample.scala
+++ day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/ConcurrencyExample.scala
@@ -1,5 +1,8 @@
 package org.frontierdevelopers.scalaworkshop.day2.session2
 
+import akka.actor.Actor
+import akka.actor.Actors._;
+
 /**
  * @author Sam Reid
  */
@@ -7,5 +10,17 @@ package org.frontierdevelopers.scalaworkshop.day2.session2
 object ConcurrencyExample {
   def main(args: Array[String]) = {
     println("hello")
+
+    // server code
+    class HelloWorldActor extends Actor {
+      def receive = {
+        case msg => self reply ( msg + " World" )
+      }
+    }
+    remote.start("localhost", 9999).register("hello-service", actorOf(classOf[HelloWorldActor]))
+
+    // client code
+    val actor = remote.actorFor("hello-service", "localhost", 9999)
+    val result = actor !! "Hello"
   }
 }
