61a00fa9a39c01801b3535fbb5655e10b7a4dbd3
Kris Nuttycombe <kris@hylotech.com>
2011-04-09 12:27:34 -0600

    Added IDEA support in SBT

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) {
+class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
   val scala_swing = "org.scala-lang" % "scala-swing" % "2.8.1"
 }
 
