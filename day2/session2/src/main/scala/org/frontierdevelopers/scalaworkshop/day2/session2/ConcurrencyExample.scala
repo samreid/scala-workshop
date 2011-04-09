9812214c8fcb756e92958cf22f9fb237c7619274
Sam Reid <reids@colorado.edu>
2011-04-09 13:55:15 -0600

    improved actor example

--- day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/ConcurrencyExample.scala
+++ day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/ConcurrencyExample.scala
@@ -14,13 +14,27 @@ object ConcurrencyExample {
     // server code
     class HelloWorldActor extends Actor {
       def receive = {
-        case msg => self reply ( msg + " World" )
+        case msg => {
+          println("hello there, message received")
+          self reply ( msg + " World" )
+        }
       }
     }
     remote.start("localhost", 9999).register("hello-service", actorOf(classOf[HelloWorldActor]))
 
     // client code
     val actor = remote.actorFor("hello-service", "localhost", 9999)
-    val result = actor !! "Hello"
+    val result = actor !! "Hello afterwanrds"
+    println(result)
+
+  }
+}
+
+object Example2 {
+  def main(args: Array[String]) {
+    println("hello2")
+    val x = Map("sword"->123)
+    val y = x.toList.sortWith( _._2 < _._2 )
+    println("y = "+y)
   }
 }
