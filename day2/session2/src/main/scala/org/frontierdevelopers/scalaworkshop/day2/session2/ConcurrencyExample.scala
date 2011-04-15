12b7dd9ffdb3e8137bfd9dd4b34d222a342f8f64
Sam Reid <reids@colorado.edu>
2011-04-14 21:54:29 -0600

    updated actor examples

--- day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/ConcurrencyExample.scala
+++ day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/ConcurrencyExample.scala
@@ -54,11 +54,23 @@ object Example3 {
     val actor = actorOf(new Creator[Actor] {
       def create = new Actor {
         protected def receive = {
+          case 123 => self.reply_?("got your 123")
           case x: String => println("received string: " + x)
         }
       }
     })
     actor.start()
     actor.sendOneWay("hello there, second time")
+    actor.sendOneWay(123.asInstanceOf[AnyRef])
+    actor ! "hello !"
+    ( actor !! 123.asInstanceOf[AnyRef] ) match {
+      case Some(x: String) => println(" got response " + x)
+      case Some(false) => println(" got response false")
+      case None => println("none")
+    }
+
+    val future = actor !!! 123.asInstanceOf[AnyRef]
+    val result = future
+    println(result)
   }
 }
