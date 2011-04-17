9c1e834d2a345b9546fa7e62673212ca209c456a
Sam Reid <reids@colorado.edu>
2011-04-16 20:50:47 -0600

    started reorganizing examples

--- day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/RemoteActorExample.scala
+++ day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/RemoteActorExample.scala
@@ -2,9 +2,6 @@ package org.frontierdevelopers.scalaworkshop.day2.session2
 
 import akka.actor.Actor
 import akka.actor.Actors._
-import akka.japi.Creator
-;
-
 /**
  * @author Sam Reid
  */
@@ -29,47 +26,5 @@ object RemoteActorExample {
     val result = actor !! "Hello afterwanrds"
     println(result)
 
-  }
-}
-
-class ConsoleActor extends Actor {
-  def receive = {
-    case x: String => println("Console Actor received string: " + x)
-  }
-}
-
-object Example1LocalActor {
-  def main(args: Array[String]) {
-    val actor = actorOf(classOf[ConsoleActor])
-    actor.start()
-    actor.sendOneWay("hello there")
-    actor.sendOneWay("hello again")
-    actor ! "hello using !"
-  }
-}
-
-object Example3 {
-  def main(args: Array[String]) {
-    val actor = actorOf(new Creator[Actor] {
-      def create = new Actor {
-        protected def receive = {
-          case 123 => self reply_? "got your 123"
-          case x: String => println("received string: " + x)
-        }
-      }
-    })
-    actor.start()
-    actor.sendOneWay("hello there, second time")
-    actor.sendOneWay(123.asInstanceOf[AnyRef])
-    actor ! "hello !"
-    ( actor !! 123.asInstanceOf[AnyRef] ) match {
-      case Some(x: String) => println(" got response " + x)
-      case Some(false) => println(" got response false")
-      case None => println("none")
-    }
-
-    val future = actor !!! 123.asInstanceOf[AnyRef]
-    val result = future
-    println(result)
   }
 }
