c3a5bcf10b10edda17b306868f9dd727ea6ce354
Sam Reid <reids@colorado.edu>
2011-04-16 21:22:26 -0600

    added examples for remote actors

--- day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/Example4RemoteActorSameJVM.scala
+++ day2/session2/src/main/scala/org/frontierdevelopers/scalaworkshop/day2/session2/Example4RemoteActorSameJVM.scala
@@ -2,14 +2,9 @@ package org.frontierdevelopers.scalaworkshop.day2.session2
 
 import akka.actor.Actor
 import akka.actor.Actors._
-/**
- * @author Sam Reid
- */
 
-object RemoteActorExample {
+object Example4RemoteActorSameJVM {
   def main(args: Array[String]) {
-    println("hello")
-
     // server code
     class HelloWorldActor extends Actor {
       def receive = {
@@ -23,8 +18,7 @@ object RemoteActorExample {
 
     // client code
     val actor = remote.actorFor("hello-service", "localhost", 9999)
-    val result = actor !! "Hello afterwanrds"
+    val result = actor !! "Hello afterwards"
     println(result)
-
   }
 }
