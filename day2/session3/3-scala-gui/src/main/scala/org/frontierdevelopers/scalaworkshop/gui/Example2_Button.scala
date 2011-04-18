9061cd52eee255ab016b796e1e25989906f46bc0
Sam Reid <reids@colorado.edu>
2011-04-17 21:02:23 -0600

    renamed example

--- day2/session3/3-scala-gui/src/main/scala/org/frontierdevelopers/scalaworkshop/gui/Example2_Button.scala
+++ day2/session3/3-scala-gui/src/main/scala/org/frontierdevelopers/scalaworkshop/gui/Example2_Button.scala
@@ -2,7 +2,7 @@ package org.frontierdevelopers.scalaworkshop.gui
 
 import swing.{Button, MainFrame, SimpleSwingApplication}
 
-object Example1_ButtonInteraction extends SimpleSwingApplication {
+object Example2_Button extends SimpleSwingApplication {
   def top = new MainFrame {
     contents = Button("Hello"){
       println("button pressed")
