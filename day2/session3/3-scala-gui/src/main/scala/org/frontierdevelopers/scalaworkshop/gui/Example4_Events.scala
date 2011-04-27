9eb2e43cc069134d29417943db84e85fb6a260d3
Sam Reid <reids@colorado.edu>
2011-04-26 23:34:07 -0600

    added event example

--- day2/session3/3-scala-gui/src/main/scala/org/frontierdevelopers/scalaworkshop/gui/Example4_Events.scala
+++ day2/session3/3-scala-gui/src/main/scala/org/frontierdevelopers/scalaworkshop/gui/Example4_Events.scala
@@ -1,5 +1,23 @@
 package org.frontierdevelopers.scalaworkshop.gui
 
-object Example4_Events {
+import swing._
+import event.{ValueChanged, ButtonClicked}
 
+object Example4_Events extends SimpleSwingApplication {
+  def top = new MainFrame {
+    contents = new BoxPanel(Orientation.Vertical) {
+      val button = new Button {text = "A button"}
+      contents += button
+      val slider = new Slider
+      contents += slider
+      val textField = new TextField {text = "a text field"}
+      contents += textField
+      listenTo(button)
+      listenTo(slider)
+      reactions += {
+        case ButtonClicked(b) => textField.text = "Button pressed!"
+        case ValueChanged(s: Slider) => textField.text = "Slider value: " + s.value
+      }
+    }
+  }
 }
